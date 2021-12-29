ca29166caf7f741f5de3ae5fec2d903e61f9d70d
Spotts91 <oakshassa864@gmail.com>
2021-12-28 18:25:50 -0800

    question and answer section coded

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,51 @@ var enterInitialsTextAreaEl = document.createElement("enterInitialsTextArea");
 var button1234 = document.createElement("button");
 var timeLeft = 60;
 
+// Do not diplay anything that is not ready to be displayed
+answer1BtnEl.style.display = 'none';
+answer2BtnEl.style.display = 'none';
+answer3BtnEl.style.display = 'none';
+answer4BtnEl.style.display = 'none';
+submitScoreEl.style.display = 'none';
+answerCorrectWrong.style.display = 'none';
+enterInitialsTextArea.style.display = 'none';
 
+var questionsObject = { // Object that holds correct answer.
+    correct:{
+        0 : "Commonly used datatypes DO NOT include?",
+        1 : "The condition statement if/else is enclosed with the following:",
+        2 : "Arrays can be used to store the following",
+        3 : "A very useful tool to debug arrays is:",
+        4 : "Strings must be enclosed with:"
+    }
+};
+
+var answerObject = {// Object that holds correct answers
+    answers: {
+        0 : {
+            0:"Strings",
+            1:"Boolean",
+            2:"Alerts",
+            3:"Numbers"},
+        1 : {
+            0: "Parentheses",
+            1: "Curly Brackets",
+            2: "Quotes",
+            3: "Square Brackets"},
+        2 : {
+            0: "Javascript",
+            1: "Terminal/bash",
+            2: "For Loops",
+            3: "Console.log"},
+        3 : {
+            0: "Commas",
+            1: "Curly Brackets",
+            2: "Quotes",
+            3: "Parentheses"},
+        4 : {
+            0: "Number of strings",
+            1: "Other arrays",
+            2: "Booleans",
+            3: "All of the above"},
+    }
+};
